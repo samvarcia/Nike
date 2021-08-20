60d6351ee3bb2237bac1cba3fbaecdd461be602c
Samuel Garcia <samfrontdev@gmail.com>
2021-08-20 16:44:45 -0500

    🎉 Nike project initialized

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../assets/styles/main.css";
+
+class App extends Component {
+  render() {
+    return <h1>NIKE</h1>;
+  }
+}
+
+export default App;
